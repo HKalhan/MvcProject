18a72e280c39601d8eb7cce02619f447a3113f3d
HKalhan <hasre.kalhann@gamil.com>
2021-04-28 01:30:01 +0300

    first commit

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+
+namespace Business.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+
+        public void CategoryAddBL (Category entity)
+        {
+            if(entity.CategoryName=="" || entity.CategoryName.Length <=3 ||
+                entity.CategoryDescription=="" || entity.CategoryName.Length >= 51)
+            {
+                //error message
+            }
+            else
+            {
+                repo.Insert(entity);
+            }
+        }    
+    }
+
+}
